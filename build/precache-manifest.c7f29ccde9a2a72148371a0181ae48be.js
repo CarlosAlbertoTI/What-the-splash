5f7b3ce84f816fffc9f892529b1da1d4d1c3707e
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2022-10-09 13:20:45 -0300

    adicionando build

---
+++ build/precache-manifest.c7f29ccde9a2a72148371a0181ae48be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e56594c0b07d97ca972d5b9ed42afae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "659a5a02fdbf14eda66a",
+    "url": "/static/css/main.36b243f6.chunk.css"
+  },
+  {
+    "revision": "2297babc53654ac36993",
+    "url": "/static/js/2.8641802e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8641802e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659a5a02fdbf14eda66a",
+    "url": "/static/js/main.5a166f90.chunk.js"
+  },
+  {
+    "revision": "b0b1f87a436646524082",
+    "url": "/static/js/runtime-main.74e9e161.js"
+  }
+]);
